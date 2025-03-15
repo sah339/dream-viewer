c763d35024367254e3e3e995825169a41b448a8b
Stan <129595643+sah339@users.noreply.github.com>
2025-03-15 18:42:33 +0000

    Inited shadcn, added resizeable component and setup resizeable main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import Link from "next/link";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "~/components/ui/resizable"
+
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div>
-        
+    <main className="flex min-h-screen items-center justify-center bg-neutral-800 text-white">
+      <div className="w-screen h-screen">
+        <ResizablePanelGroup direction="horizontal">
+          <ResizablePanel className="flex items-center justify-center">One</ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel className="flex items-center justify-center">Two</ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </main>
   );
